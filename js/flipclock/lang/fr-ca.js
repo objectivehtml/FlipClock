9d98f60dc7deb77ecedde3604228079c8880df46
Melissa P <m@melz.org>
2013-11-20 20:34:23 -0500

    Added Canadian French translation

---
+++ js/flipclock/lang/fr-ca.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Canadian French Language Pack
+   *
+   * This class will used to translate tokens into the Canadian French language.
+   *
+   */
+
+  FlipClock.Lang.French = {
+
+    'years'   : 'ans',
+    'months'  : 'mois',
+    'days'    : 'jours',
+    'hours'   : 'heures',
+    'minutes' : 'minutes',
+    'seconds' : 'secondes'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['fr']      = FlipClock.Lang.French;
+  FlipClock.Lang['fr-ca']   = FlipClock.Lang.French;
+  FlipClock.Lang['french']  = FlipClock.Lang.French;
+
+}(jQuery));
