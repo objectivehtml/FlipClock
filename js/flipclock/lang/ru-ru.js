4e13df87337fe2f15b6e9e51d50c149824090bb3
PureMango <stalker1894@gmail.com>
2013-12-29 12:08:20 +0600

    Russian translation
    Russian language pack

---
+++ js/flipclock/lang/ru-ru.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Russian Language Pack
+   *
+   * This class will used to translate tokens into the Russian language.
+   *
+   */
+
+  FlipClock.Lang.French = {
+
+    'years'   : 'лет',
+    'months'  : 'месяцев',
+    'days'    : 'дней',
+    'hours'   : 'часов',
+    'minutes' : 'минут',
+    'seconds' : 'секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['ru']      = FlipClock.Lang.French;
+  FlipClock.Lang['ru-ru']   = FlipClock.Lang.French;
+  FlipClock.Lang['russian']  = FlipClock.Lang.French;
+
+}(jQuery));
