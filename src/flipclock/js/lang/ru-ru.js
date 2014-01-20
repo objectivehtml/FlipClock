5c7311b0432c0cc544b65ad38f10cf7109d6253a
Justin Kimbrell <justin@objectivehtml.com>
2014-01-20 12:02:52 -0500

    - (Feature) Added Russian language pack

---
+++ src/flipclock/js/lang/ru-ru.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Russian Language Pack
+   *
+   * This class will used to translate tokens into the Russian language.
+   *
+   */
+
+  FlipClock.Lang.Russian = {
+
+    'years'   : 'лет',
+    'months'  : 'месяцев',
+    'days'    : 'дней',
+    'hours'   : 'часов',
+    'minutes' : 'минут',
+    'seconds' : 'секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['ru']      = FlipClock.Lang.Russian;
+  FlipClock.Lang['ru-ru']   = FlipClock.Lang.Russian;
+  FlipClock.Lang['russian']  = FlipClock.Lang.Russian;
+
+}(jQuery));
