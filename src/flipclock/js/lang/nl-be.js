919ede32f52bec87c87e4aef34c26fdc4375b50b
Wim Symons <wim.symons@gmail.com>
2014-03-05 09:25:09 +0100

    Added Dutch language

---
+++ src/flipclock/js/lang/nl-be.js
@@ -0,0 +1,26 @@
+(function($) {
+
+    /**
+     * FlipClock Dutch Language Pack
+     *
+     * This class will used to translate tokens into the Dutch language.
+     */
+
+    FlipClock.Lang.Dutch = {
+
+        'years'   : 'Jaren',
+        'months'  : 'Maanden',
+        'days'    : 'Dagen',
+        'hours'   : 'Uren',
+        'minutes' : 'Minuten',
+        'seconds' : 'Seconden'
+
+    };
+
+    /* Create various aliases for convenience */
+
+    FlipClock.Lang['nl']      = FlipClock.Lang.Dutch;
+    FlipClock.Lang['nl-be']   = FlipClock.Lang.Dutch;
+    FlipClock.Lang['dutch']   = FlipClock.Lang.Dutch;
+
+}(jQuery));
