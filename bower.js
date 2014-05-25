459be90b4d93011656678774026be5bc7ae697f6
Justin Kimbrell <justin@objectivehtml.com>
2014-05-25 19:41:12 -0400

    Added bower.js file to the core repo

---
+++ bower.js
@@ -0,0 +1,19 @@
+{
+  "name": "flipclock",
+  "version": "0.5.0",
+  "homepage": "https://github.com/objectivehtml/FlipClock",
+  "authors": [
+    "Objective HTML",
+    "Justin Kimbrell"
+  ],
+  "description": "FlipClock.js is a proper abstract clock and counter library featuring a realisting flipping effect.",
+  "main": "compiled/flipclock.js",
+  "license": "MIT",
+  "ignore": [
+    ".jshintrc",
+    "bower_components"
+  ],
+  "dependencies": {
+    "jquery": ">=1.7.0"
+  }
+}
