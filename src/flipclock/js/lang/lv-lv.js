c7878c0182dfe43ff3289fea989e8144d57b3279
Jānis Liepa <janis.liepa@tieto.com>
2014-06-18 18:00:23 +0300

    Feature: added latvian language to flip clock labels
    To support UTF-8 add <meta charset="utf-8">

---
+++ src/flipclock/js/lang/lv-lv.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Latvian Language Pack
+   *
+   * This class will used to translate tokens into the Latvian language.
+   *
+   */
+
+  FlipClock.Lang.Latvian = {
+
+    'years'   : 'Gadi',
+    'months'  : 'Mēneši',
+    'days'    : 'Dienas',
+    'hours'   : 'Stundas',
+    'minutes' : 'Minūtes',
+    'seconds' : 'Sekundes'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['lv']      = FlipClock.Lang.Latvian;
+  FlipClock.Lang['lv-lv']   = FlipClock.Lang.Latvian;
+  FlipClock.Lang['latvian'] = FlipClock.Lang.Latvian;
+
+}(jQuery));
