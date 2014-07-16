0690e831601b17fcd2f9a4d1d869ca640906ae0f
marcker <marckfree@gmail.com>
2014-07-16 10:02:30 -0300

    Removes duplicate code

--- compiled/flipclock.js
+++ compiled/flipclock.js
@@ -2399,33 +2399,6 @@ var FlipClock;
 }(jQuery));
 (function($) {
 
-	/**
-	 * FlipClock Portuguese Language Pack
-	 *
-	 * This class will used to translate tokens into the Portuguese language.
-	 *
-	 */
-
-	FlipClock.Lang.Portuguese = {
-
-		'years'   : 'Anos',
-		'months'  : 'Meses',
-		'days'    : 'Dias',
-		'hours'   : 'Horas',
-		'minutes' : 'Minutos',
-		'seconds' : 'Segundos'
-
-	};
-	
-	/* Create various aliases for convenience */
-
-	FlipClock.Lang['pt']         = FlipClock.Lang.Portuguese;
-	FlipClock.Lang['pt-br']      = FlipClock.Lang.Portuguese;
-	FlipClock.Lang['portuguese'] = FlipClock.Lang.Portuguese;
-
-}(jQuery));
-(function($) {
-
   /**
    * FlipClock Canadian French Language Pack
    *
