393efe5b399b0a8863cff0e9e1a7cc6890e28cb4
Vegar Engen <vegar.engen@gmail.com>
2014-07-23 02:39:17 +0200

    Added Norwegian Bokmål Language Pack

---
+++ src/flipclock/js/lang/no-nb.js
@@ -0,0 +1,28 @@
+(function($) {
+
+	/**
+	 * FlipClock Norwegian-Bokmål Language Pack
+	 *
+	 * This class will used to translate tokens into the Norwegian language.
+	 *	
+	 */
+
+	FlipClock.Lang.Norwegian-BM = {
+
+		'years'   : 'År',
+		'months'  : 'Måneder',
+		'days'    : 'Dager',
+		'hours'   : 'Timer',
+		'minutes' : 'Minutter',
+		'seconds' : 'Sekunder'	
+
+	};
+
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['no']      = FlipClock.Lang.Norwegian-BM;
+	FlipClock.Lang['nb']      = FlipClock.Lang.Norwegian-BM;
+	FlipClock.Lang['no-nb']   = FlipClock.Lang.Norwegian-BM;
+	FlipClock.Lang['norwegian'] = FlipClock.Lang.Norwegian-BM;
+
+}(jQuery));
