71455fd86c688cda19085d21840e62aedbdf8052
Justin Kimbrell <justin@objectivehtml.com>
2014-12-12 15:53:19 -0500

    Recompiled source

--- compiled/flipclock.js
+++ compiled/flipclock.js
@@ -197,13 +197,13 @@ var FlipClock;
 		 * Build Date
 		 */
 		 
-		buildDate: '2014-10-06',
+		buildDate: '2014-12-12',
 		
 		/**
 		 * Version
 		 */
 		 
-		version: '0.7.5',
+		version: '0.7.7',
 		
 		/**
 		 * Sets the default options
@@ -447,7 +447,7 @@ var FlipClock;
 
 		reset: function() {
 			this.factory.time = new FlipClock.Time(
-				this.factor, 
+				this.factory, 
 				this.factory.original ? Math.round(this.factory.original) : 0,
 				{
 					minimumDigits: this.factory.minimumDigits
@@ -2490,7 +2490,7 @@ var FlipClock;
 		
 		'years'   : 'A&#241;os',
 		'months'  : 'Meses',
-		'days'    : 'D&#237;as',
+		'days'    : 'D&#205;as',
 		'hours'   : 'Horas',
 		'minutes' : 'Minutos',
 		'seconds' : 'Segundo'	
