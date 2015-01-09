d09ad948238397f383003cc9026ede2f34baf167
Justin Kimbrell <justin@objectivehtml.com>
2015-01-09 03:05:24 -0500

    - (API) Renamed Minute Counter Face file

---
+++ src/flipclock/js/faces/MinuteCounterFace.js
@@ -0,0 +1,40 @@
+(function($) {
+		
+	/**
+	 * Minute Counter Clock Face
+	 *
+	 * This class will generate a minute counter for FlipClock.js. A
+	 * minute counter will track minutes and seconds. If an hour is 
+	 * reached, the counter will reset back to 0. (4 digits max)
+	 *	
+	 */
+	 
+	FlipClock.MinuteCounterFace = FlipClock.HourlyCounterFace.extend({
+
+		/**
+		 * Build the clock face
+		 *	
+		 * @return
+		 */
+		 
+		build: function() {
+			this.base(true, this.time.getMinuteCounter());
+		},
+		
+		/**
+		 * Flip the clock face
+		 *	
+		 * @return
+		 */
+		 
+		flip: function(time) {
+			if(!time) {
+				time = this.time.getMinuteCounter();
+			}
+
+			this.base(time);
+		}
+
+	});
+	
+}(jQuery));
