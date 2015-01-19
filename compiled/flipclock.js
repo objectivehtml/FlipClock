17697c0891bd8e7908db15c613fee96f99b44036
Justin Kimbrell <justin@objectivehtml.com>
2015-01-19 01:57:31 -0500

    - (Feature) Added Chinese language file

--- compiled/flipclock.js
+++ compiled/flipclock.js
@@ -2862,6 +2862,233 @@ var FlipClock;
 	});
 
 }(jQuery));
+/*jshint smarttabs:true */
+
+/**
+ * FlipClock.js
+ *
+ * @author     Justin Kimbrell
+ * @copyright  2013 - Objective HTML, LLC
+ * @licesnse   http://www.opensource.org/licenses/mit-license.php
+ */
+
+(function($) {
+	
+	"use strict";
+	
+	/**
+	 * The FlipClock.List class is used to build the list used to create 
+	 * the card flip effect. This object fascilates selecting the correct
+	 * node by passing a specific value.
+	 */
+	 	
+	FlipClock.List = FlipClock.Base.extend({
+			
+		/**
+		 * The jQuery object
+		 */		
+		 
+		$el: false,
+		
+		/**
+		 * The items in the list
+		 */		
+		 
+		items: [],
+		
+		/**
+		 * The available options for this class
+		 */		
+		
+		options: {
+
+			/**
+			 * The CSS classes
+			 */		
+			 
+			classes: {
+				active: 'flip-clock-active',
+				before: 'flip-clock-before',
+				flip: 'flip',
+				play: 'play'
+			},
+				
+			/**
+			 * The last value selected in the list
+			 */		
+			 
+			lastValue: 0
+		},
+
+		/**
+		 * The selected value in the list
+		 */		
+		 
+		value: 0,
+		
+		/**
+		 * Constructor
+		 *
+		 * @param  object  A FlipClock.Factory object
+		 * @param  int     An string or integer use to select the correct value
+		 * @param  object  An object to override the default properties	 
+		 */
+		 
+		constructor: function(value, options) {
+			this.base(options);
+
+			this.value = value;
+
+			var t = this;
+
+			this.createList();
+
+			this.trigger('init');
+		},
+		
+		/**
+		 * Select the value in the list
+		 *
+		 * @param  int  A value 0-9	 
+		 */
+		 
+		select: function(value) {
+			var _afterListItem = this._afterListItem;
+
+			this.setOption('lastValue', this.value);
+
+			if(typeof value === "undefined") {
+				value = this.value;
+			}
+			else {
+				this.value = value;
+			}
+
+			if(this.value != this.getOption('lastValue')) {
+				this._beforeListItem.$el.removeClass(this.getOption('classes').before);
+
+				this.$el.find('.'+this.getOption('classes').active)
+					.removeClass(this.getOption('classes').active)
+					.addClass(this.getOption('classes').before);
+
+				this.items.splice(0, 1);
+
+				this._afterListItem = this.createListItem(this.value, this.getOption('classes').active);
+
+				this._beforeListItem.$el.remove();
+				this._beforeListItem = _afterListItem;
+
+				this.trigger('select', this.value);
+			}	
+		},
+
+		/*
+		 * Add the play class to the list
+		 *
+		 * @return 
+		*/
+
+		addPlayClass: function() {
+			this.$el.addClass(this.getOption('classes').play);
+		},
+		
+		/*
+		 * Remove the play class to the list
+		 *
+		 * @return 
+		*/
+
+		removePlayClass: function() {
+			this.$el.removeClass(this.getOption('classes').play);
+		},
+		
+		/**
+		 * Creates the list item HTML and returns as a string 
+		 */
+		 
+		createListItem: function(value, css) {
+			var item = new FlipClock.ListItem(value, {
+				className: css
+			});
+
+			this.items.push(item);
+
+			this.$el.append(item.$el);
+
+			this.trigger('create:item', item);
+
+			return item;
+		},
+
+		/**
+		 * Create the list of values and appends it to the DOM object 
+		 */
+		 
+		createList: function() {
+			var $el = this.$el = $('<ul class="'+this.getOption('classes').flip+'"></ul>');
+
+			this._beforeListItem = this.createListItem(this.getPrevValue(), this.getOption('classes').before);
+			this._afterListItem = this.createListItem(this.value, this.getOption('classes').active);
+
+			$el.append(this._beforeListItem.el);
+			$el.append(this._afterListItem.el);
+			
+			this.trigger('create:list', $el);		
+
+			return $el;
+		}
+
+	});
+		
+}(jQuery));
+/*jshint smarttabs:true */
+
+/**
+ * FlipClock.js
+ *
+ * @author     Justin Kimbrell
+ * @copyright  2013 - Objective HTML, LLC
+ * @licesnse   http://www.opensource.org/licenses/mit-license.php
+ */
+
+(function($) {
+	
+	"use strict";
+	
+	/**
+	 * Capitalize the first letter in a string
+	 *
+	 * @return string
+	 */
+	 
+	String.prototype.ucfirst = function() {
+		return this.substr(0, 1).toUpperCase() + this.substr(1);
+	};
+	
+	/**
+	 * jQuery helper method
+	 *
+	 * @param  int     An integer used to start the clock (no. seconds)
+	 * @param  object  An object of properties to override the default	
+	 */
+	 
+	$.fn.FlipClock = function(digit, options) {	
+		return new FlipClock($(this), digit, options);
+	};
+	
+	/**
+	 * jQuery helper method
+	 *
+	 * @param  int     An integer used to start the clock (no. seconds)
+	 * @param  object  An object of properties to override the default	
+	 */
+	 
+	$.fn.flipClock = function(digit, options) {
+		return $.fn.FlipClock(digit, options);
+	};
+	
+}(jQuery));
+
 (function($) {
 	
 	/**
@@ -3779,5 +4006,33 @@ var FlipClock;
 	FlipClock.Lang['sv']      = FlipClock.Lang.Swedish;
 	FlipClock.Lang['sv-se']   = FlipClock.Lang.Swedish;
 	FlipClock.Lang['swedish'] = FlipClock.Lang.Swedish;
+
+}(jQuery));
+
+(function($) {
+		
+	/**
+	 * FlipClock Chinese Language Pack
+	 *
+	 * This class will used to translate tokens into the Chinese language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Chinese = {
+		
+		'years'   : '年',
+		'months'  : '月',
+		'days'    : '日',
+		'hours'   : '时',
+		'minutes' : '分',
+		'seconds' : '秒'
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['zh']      = FlipClock.Lang.Chinese;
+	FlipClock.Lang['zh-cn']   = FlipClock.Lang.Chinese;
+	FlipClock.Lang['chinese'] = FlipClock.Lang.Chinese;
 
 }(jQuery));
---
+++ src/flipclock/js/lang/zh-cn.js
@@ -0,0 +1,27 @@
+(function($) {
+		
+	/**
+	 * FlipClock Chinese Language Pack
+	 *
+	 * This class will used to translate tokens into the Chinese language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Chinese = {
+		
+		'years'   : '年',
+		'months'  : '月',
+		'days'    : '日',
+		'hours'   : '时',
+		'minutes' : '分',
+		'seconds' : '秒'
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['zh']      = FlipClock.Lang.Chinese;
+	FlipClock.Lang['zh-cn']   = FlipClock.Lang.Chinese;
+	FlipClock.Lang['chinese'] = FlipClock.Lang.Chinese;
+
+}(jQuery));
