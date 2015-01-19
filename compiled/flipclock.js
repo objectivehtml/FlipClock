39c0246ef8c6846bb4200438e51bb1c732218fa7
Justin Kimbrell <justin@objectivehtml.com>
2015-01-19 01:59:06 -0500

    Recompiled source

--- compiled/flipclock.js
+++ compiled/flipclock.js
@@ -2752,3 +2752,31 @@ var FlipClock;
 	FlipClock.Lang['swedish'] = FlipClock.Lang.Swedish;
 
 }(jQuery));
+
+(function($) {
+		
+	/**
+	 * FlipClock Chinese Language Pack
+	 *
+	 * This class will used to translate tokens into the Chinese language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Chinese = {
+		
+		'years'   : '年',
+		'months'  : '月',
+		'days'    : '日',
+		'hours'   : '时',
+		'minutes' : '分',
+		'seconds' : '秒'
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['zh']      = FlipClock.Lang.Chinese;
+	FlipClock.Lang['zh-cn']   = FlipClock.Lang.Chinese;
+	FlipClock.Lang['chinese'] = FlipClock.Lang.Chinese;
+
+}(jQuery));
