5ab8ecf1ae1b08b22ff60a6fe6fa3f9dc6ffebc3
mirkinoid <myroslav.tkachenko@gmail.com>
2015-03-17 18:28:34 +0200

    Add Ukrainian language pack. Recompile

--- compiled/flipclock.js
+++ compiled/flipclock.js
@@ -2754,6 +2754,33 @@ var FlipClock;
 }(jQuery));
 
 (function($) {
+
+  /**
+   * FlipClock Ukrainian Language Pack
+   *
+   * This class will used to translate tokens into the Ukrainian language.
+   *
+   */
+
+  FlipClock.Lang.Ukrainian = {
+
+    'years'   : 'Років',
+    'months'  : 'Місяців',
+    'days'    : 'Днів',
+    'hours'   : 'Годин',
+    'minutes' : 'Хвилин',
+    'seconds' : 'Секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['ua']      = FlipClock.Lang.Ukrainian;
+  FlipClock.Lang['ua-ua']   = FlipClock.Lang.Ukrainian;
+  FlipClock.Lang['ukrainian']  = FlipClock.Lang.Ukrainian;
+
+}(jQuery));
+(function($) {
 		
 	/**
 	 * FlipClock Chinese Language Pack
