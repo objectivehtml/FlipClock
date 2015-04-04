5723af78456510521e45678bce68c26022a17ddd
Neuronus <piotr@interacti-labs.pl>
2015-04-04 23:13:41 +0200

    Polish language pack added

---
+++ src/flipclock/js/lang/pl-pl.js
@@ -0,0 +1,27 @@
+(function($) {
+
+	/**
+	 * FlipClock Polish Language Pack
+	 *
+	 * This class will used to translate tokens into the Polish language.
+	 * 
+	 */
+
+	FlipClock.Lang.Portuguese = {
+
+		'years'   : 'Lat',
+		'months'  : 'Miesiêcy',
+		'days'    : 'Dni',
+		'hours'   : 'Godzin',
+		'minutes' : 'Minut',
+		'seconds' : 'Sekund'
+
+	};
+
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['pl']       	= FlipClock.Lang.Polish;
+	FlipClock.Lang['pl-pl']     = FlipClock.Lang.Polish;
+	FlipClock.Lang['polish'] 	= FlipClock.Lang.Polish;
+
+}(jQuery));
