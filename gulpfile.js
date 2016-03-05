03136fdb35142de83c35bf41289ff35fa3f5f679
Brian Espinosa <brian@getplayerpro.com>
2016-03-04 21:33:52 -0800

    #261 added error handling to sass / persist watch
    Signed-off-by: Brian Espinosa <brian@getplayerpro.com>

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,41 @@ var
   uglify            = require('gulp-uglify');
   concat            = require('gulp-concat');
   notify            = require('gulp-notify');
+  gutil             = require('gulp-util');
   rename            = require('gulp-rename');
   pkg               = require('./package.json');
 ;
+
+var reportError = function (error) {
+  var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+
+  notify({
+      title: 'Task Failed [' + error.plugin + ']',
+      message: lineNumber + 'See console.',
+      sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+  }).write(error);
+
+  gutil.beep(); // Beep 'sosumi' again
+
+  // Inspect the error object
+  //console.log(error);
+
+  // Easy error reporting
+  //console.log(error.toString());
+
+  // Pretty error reporting
+  var report = '';
+  var chalk = gutil.colors.white.bgRed;
+
+  report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+  report += chalk('PROB:') + ' ' + error.message + '\n';
+  if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+  if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+  console.error(report);
+
+  // Prevent the 'watch' task from stopping
+  this.emit('end');
+}
 
 var banner = [
   '/* <%= pkg.name %> - v<%= pkg.version %> */',
@@ -62,7 +94,7 @@ gulp.task('dist', function () {
 gulp.task('sass', function() {
   return gulp.src("src/flipclock/scss/**/*.scss")
     .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(sass({outputStyle: 'compressed'}).on('error', reportError))
       .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
