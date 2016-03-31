b63d86118701d627c2a504e6d289aa5101535d85
Justin Kimbrell <justin@objectivehtml.com>
2016-03-31 18:52:30 -0400

    Added Hebrew Language

---
+++ src/flipclock/js/lang/he-il.js
@@ -0,0 +1,25 @@
+(function($) {
+
+	/**
+	FlipClock Hebrew Language Pack *
+	This class will used to translate tokens into the Hebrew language. *
+	Laurent HADJADJ - 10/09/2015
+	*/
+
+	FlipClock.Lang.Hebrew = {
+		'years' : 'שנים',
+		'months' : 'חודש',
+		'days' : 'ימים',
+		'hours' : 'שעות',
+		'minutes' : 'דקות',
+		'seconds' : 'שניות'
+
+	};
+
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['il'] = FlipClock.Lang.Hebrew;
+	FlipClock.Lang['he-il'] = FlipClock.Lang.Hebrew;
+	FlipClock.Lang['hebrew'] = FlipClock.Lang.Hebrew;
+
+}(jQuery));
