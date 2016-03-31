3df2ca2297aeaf4bf83c8c66caa19688787e98d5
Justin Kimbrell <justin@objectivehtml.com>
2016-03-31 18:45:39 -0400

    Added Persian Language

---
+++ src/flipclock/js/lang/fa-ir.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+	/**
+	 * FlipClock Persian Language Pack
+	 *
+	 * This class will used to translate tokens into the Persian language.
+	 *  
+	 */
+
+	FlipClock.Lang.Persian = {
+
+	    'years': 'سال',
+	    'months': 'دقیقه',
+	    'days': 'روز',
+	    'hours': 'ساعت',
+	    'minutes': 'دقیقه',
+	    'seconds': 'ثانیه'
+
+	};
+
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['fa'] = FlipClock.Lang.Persian;
+	FlipClock.Lang['fa-ir'] = FlipClock.Lang.Persian;
+	FlipClock.Lang['persian'] = FlipClock.Lang.Persian;
+
+}(jQuery));
