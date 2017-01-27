c905f014a9a7e690e3d9ab27633fea5236adb95b
Ori Efrati <Ori@Oris-MacBook-Pro-2.local>
2017-01-27 14:36:29 +0200

    Hebrew language pack

---
+++ src/flipclock/js/lang/he-he.js
@@ -0,0 +1,27 @@
+(function($) {
+		
+	/**
+	 * FlipClock Hebrew Language Pack
+	 *
+	 * This class will used to translate tokens into the Hebrew language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Hebrew = {
+		
+		'years'   : 'שנים',
+		'months'  : 'חודשים',
+		'days'    : 'ימים',
+		'hours'   : 'שעות',
+		'minutes' : 'דקות',
+		'seconds' : 'שניות'	
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['he']      = FlipClock.Lang.English;
+	FlipClock.Lang['he-he']   = FlipClock.Lang.English;
+	FlipClock.Lang['hebrew'] = FlipClock.Lang.English;
+
+}(jQuery));
