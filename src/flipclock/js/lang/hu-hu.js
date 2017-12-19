1f2132551b09cf24b06f845b47450c7d23414640
Matej Kriz (mattez) <kriz@codelab.cz>
2017-12-19 12:50:47 +0100

    Fix typing error in HU lang file.

--- src/flipclock/js/lang/hu-hu.js
+++ src/flipclock/js/lang/hu-hu.js
@@ -8,20 +8,20 @@
 	 */
 	 
 	FlipClock.Lang.German = {
-		
+
 		'years'   : 'év',
-    'months'  : 'hónap',
-    'days'    : 'nap',
-    'hours'   : 'óra',
-    'minutes' : 'perc',
-    'seconds' : 'másodperc'	
- 
+		'months'  : 'hónap',
+		'days'    : 'nap',
+		'hours'   : 'óra',
+		'minutes' : 'perc',
+		'seconds' : 'másodperc'
+
 	};
 	
 	/* Create various aliases for convenience */
  
 	FlipClock.Lang['hu']     = FlipClock.Lang.German;
-	FlipClock.Lang['hu-hu]  = FlipClock.Lang.German;
+	FlipClock.Lang['hu-hu']  = FlipClock.Lang.German;
 	FlipClock.Lang['hungarian'] = FlipClock.Lang.German;
  
 }(jQuery));
