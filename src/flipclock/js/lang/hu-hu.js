93948120702073150615751a632f19e4001d96d3
Sergey Zykov <szykov@deloitte.ru>
2018-03-05 18:40:10 +0500

    Fixed synax error in #216 pull request

--- src/flipclock/js/lang/hu-hu.js
+++ src/flipclock/js/lang/hu-hu.js
@@ -1,27 +1,27 @@
-(function($) {
-		
+(function ($) {
+
 	/**
 	 * FlipClock Hungarian Language Pack
 	 *
 	 * This class will used to translate tokens into the Hungarian language.
 	 *	
 	 */
-	 
-	FlipClock.Lang.German = {
-		
-		'years'   : 'év',
-    'months'  : 'hónap',
-    'days'    : 'nap',
-    'hours'   : 'óra',
-    'minutes' : 'perc',
-    'seconds' : 'másodperc'	
- 
+
+	FlipClock.Lang.Hungarian = {
+
+		'years': 'év',
+		'months': 'hónap',
+		'days': 'nap',
+		'hours': 'óra',
+		'minutes': 'perc',
+		'seconds': 'másodperc'
+
 	};
-	
+
 	/* Create various aliases for convenience */
- 
-	FlipClock.Lang['hu']     = FlipClock.Lang.German;
-	FlipClock.Lang['hu-hu']  = FlipClock.Lang.German;
-	FlipClock.Lang['hungarian'] = FlipClock.Lang.German;
- 
+
+	FlipClock.Lang['hu'] = FlipClock.Lang.Hungarian;
+	FlipClock.Lang['hu-hu'] = FlipClock.Lang.Hungarian;
+	FlipClock.Lang['hungarian'] = FlipClock.Lang.Hungarian;
+
 }(jQuery));
