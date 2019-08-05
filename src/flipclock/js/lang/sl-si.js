0475ce4b3a7f81d412aea3ca41b608cd97e262d6
Mark Kuhar <kuharmark@gmail.com>
2019-08-05 21:34:55 +0200

    added slovenian language and fixed hungarian language

---
+++ src/flipclock/js/lang/sl-si.js
@@ -0,0 +1,27 @@
+(function($) {
+		
+	/**
+	 * FlipClock Slovenian Language Pack
+	 *
+	 * This class will used to translate tokens into the Slovenian language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Slovenian = {
+		
+		'years'   : 'Let',
+		'months'  : 'Mesecev',
+		'days'    : 'Dni',
+		'hours'   : 'Ur',
+		'minutes' : 'Minut',
+		'seconds' : 'Sekund'	
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['sl']      = FlipClock.Lang.Slovenian;
+	FlipClock.Lang['sl-si']   = FlipClock.Lang.Slovenian;
+	FlipClock.Lang['slovenian'] = FlipClock.Lang.Slovenian;
+
+}(jQuery));
