751666c530073e98d734b479cf00edded4963e87
Nguyen Quang Huy <huy9x3k@gmail.com>
2019-11-08 09:25:14 +0700

    Add Vietnamese language package

---
+++ src/flipclock/js/lang/vn-vi.js
@@ -0,0 +1,28 @@
+(function($) {
+
+  /**
+   * FlipClock Vietnamese Language Pack
+   *
+   * This class will used to translate tokens into the Vietnamese language.
+   *
+   */
+
+  FlipClock.Lang.Vietnamese = {
+
+    'years'   : 'Năm',
+    'months'  : 'Tháng',
+    'days'    : 'Ngày',
+    'hours'   : 'Giờ',
+    'minutes' : 'Phút',
+    'seconds' : 'Giây'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['vi']      = FlipClock.Lang.Vietnamese;
+  FlipClock.Lang['vn']      = FlipClock.Lang.Vietnamese;
+  FlipClock.Lang['vn-vi']   = FlipClock.Lang.Vietnamese;
+  FlipClock.Lang['vietnamese'] = FlipClock.Lang.Vietnamese;
+
+}(jQuery));
